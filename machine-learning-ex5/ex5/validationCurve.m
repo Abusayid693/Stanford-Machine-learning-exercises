64c68ad7bf8d3b9c6bea2e8602e3e39520170493
Rehane <abusayid693@gmail.com>
2021-07-09 01:21:29 +0530

    Validation curve

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -13,10 +13,35 @@ function [lambda_vec, error_train, error_val] = ...
 lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
 
 % You need to return these variables correctly.
+
 error_train = zeros(length(lambda_vec), 1);
 error_val = zeros(length(lambda_vec), 1);
 
+
+m=size(X,1)
+n=size(Xval,1)
+
+
 % ====================== YOUR CODE HERE ======================
+  for i = 1:length(lambda_vec);
+
+  theta=trainLinearReg(X,y,lambda_vec(i));
+
+ error_train(i)=(1/(2*m))*sum((X*theta-y).^2);
+ error_val(i)=(1/(2*n))*sum((Xval*theta-yval).^2);
+
+
+
+end;
+
+
+
+
+
+
+
+
+
 % Instructions: Fill in this function to return training errors in 
 %               error_train and the validation errors in error_val. The 
 %               vector lambda_vec contains the different lambda parameters 
